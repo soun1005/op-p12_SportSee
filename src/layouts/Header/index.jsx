631bcb1456e1eee0adc76ba9de2201285ff535ce
Soeun Lee <thdms1005@gmail.com>
2023-02-21 14:11:23 +0100

    folder structure and header component done

---
+++ src/layouts/Header/index.jsx
@@ -0,0 +1,20 @@
+import { ReactComponent as Logo } from '../../assets/sportseelogo.svg';
+import style from './Header.module.css';
+
+export default function Header() {
+  return (
+    <div className={style.container}>
+      <div className={style.logoContainer}>
+        <Logo className={style.logo} />
+      </div>
+      <nav className={style.nav}>
+        <ul>
+          <li>Accueil</li>
+          <li>Profil</li>
+          <li>Reglage</li>
+          <li>Communaute</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
